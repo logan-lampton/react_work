cc47ad9fb3ab39f57716a31f949a1fa17a07db5d
Logan Lampton <lampton.logan@gmail.com>
2023-01-14 18:29:35 -0500

    demonstrated adding images

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,10 +1,15 @@
-function ProfileCard({title, handle}) {
+function ProfileCard({title, handle, image}) {
    
     // destructuring in the function argument is the same as:
-    // const { title, handle } = props;
+    // const { title, handle, image } = props;
     
+    // you can console.log() the props to make sure that you put in their keys properly in the parent
+    // console.log(title, handle, image);
+
     return(
         <div>
+            {/* add in an image element to see the image prop values */}
+            <img src={image} alt="pda logo" />
             <div>Title is {title}</div>
             <div>Handle is {handle}</div>
         </div>

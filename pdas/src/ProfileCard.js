2e62840a0d4955cad6fbe64ee44eaa68a9a34112
Logan Lampton <lampton.logan@gmail.com>
2023-01-14 17:53:43 -0500

    demostrated destructuring props

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,10 +1,14 @@
-function ProfileCard(props) {
+function ProfileCard({title, handle}) {
+   
+    // destructuring in the function argument is the same as:
+    // const { title, handle } = props;
+    
     return(
         <div>
-            <div>Title is {props.title}</div>
-            <div>Handle is {props.handle}</div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
         </div>
-    )
+    );
 }
 
 export default ProfileCard;

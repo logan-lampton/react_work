d37100e1982a8f7fa255ad040b47aafbe6fb87ff
Logan Lampton <lampton.logan@gmail.com>
2023-01-17 14:35:54 -0500

    beginning animals state functionality

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+// console.log(getRandomAnimal());
+
 function App() {
 
-    const [count, setCount] = useState(0);
+    // array destructuring to set the initial state value and update it in the function call
+    const [animals, setAnimals] = useState([]);
 
     const handleClick = () => {
-        setCount(count + 1);
+        setAnimals([...animals, getRandomAnimal()]);
     }
 
     return(
@@ -13,9 +22,7 @@ function App() {
             {/* handleClick is a prop for the button below */}
             {/* handleClick is a reference; doesn't call immediately; want to call in the future when the user clicks, NOT when the app is rendered */}
             <button onClick={handleClick}>Add Animal</button>
-            <div>
-                Number of animals: {count}
-            </div>
+            <div>{animals}</div>
         </div>
     )
 }

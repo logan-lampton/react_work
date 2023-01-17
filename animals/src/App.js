5e35da3ba6f5598d0166306ed009eaa6a5d7ee0a
Logan Lampton <lampton.logan@gmail.com>
2023-01-16 19:01:42 -0500

    added setCount function and reference in App return

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,14 +1,21 @@
+import { useState } from 'react';
+
 function App() {
 
+    const [count, setCount] = useState(0);
+
     const handleClick = () => {
-        console.log('Button was clicked!');
+        setCount(count + 1);
     }
 
     return(
         <div>
             {/* handleClick is a prop for the button below */}
-            <button onClick={handleClick}>Add Animal
-            </button>
+            {/* handleClick is a reference; doesn't call immediately; want to call in the future when the user clicks, NOT when the app is rendered */}
+            <button onClick={handleClick}>Add Animal</button>
+            <div>
+                Number of animals: {count}
+            </div>
         </div>
     )
 }

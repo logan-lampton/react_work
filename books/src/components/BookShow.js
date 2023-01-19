f2415759112e8b7eaccdcd9000fedad96ea90661
Logan Lampton <lampton.logan@gmail.com>
2023-01-19 18:42:37 -0500

    added toggle to bring up component to edit book content

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
 function BookShow({ book, onDelete }) {
-    const handleClick = () => {
+    const [showEdit, setShowEdit] = useState(false);
+    
+    // make a handleDeleteClick instead of directly calling onDelete on the onClick, since onDelete needs the id of the book
+    const handleDeleteClick = () => {
         onDelete(book.id);
     };
 
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    }
+
+    // toggle logic to show component BookEdit
+    let content = <h3>{book.title}</h3>;
+    if(showEdit) {
+        content = <BookEdit />;
+    }
+
     return (
         <div className="book-show">
-            {book.title}
+            <div>{content}</div>
             <div className="actions">
-                <button className="delete" onClick={handleClick}>
+                <button className="edit" onClick={handleEditClick}>
+                    Edit
+                </button>
+                <button className="delete" onClick={handleDeleteClick}>
                     Delete
                 </button>
             </div>

dfd3b3c0039025e0426e6404dcc4c510bbb5c8ce
Logan Lampton <lampton.logan@gmail.com>
2023-01-19 14:44:10 -0500

    created groundwork for user to create a new book in the BookCreate component

--- books/src/App.js
+++ books/src/App.js
@@ -1,6 +1,18 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
 function App() {
+    // create a state array for all the books, called 'books'
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        console.log('Need to add book with title of ', title)
+    };
+
     return(
-        <div>App</div>
+        <div>
+            <BookCreate onCreate={createBook} />
+        </div>
     )
 };
 
--- books/src/components/BookCreate.js
+++ books/src/components/BookCreate.js
@@ -1,6 +1,29 @@
-function BookCreate() {
+// modify state for 'books' in the App.js component
+
+import { useState } from 'react';
+
+function BookCreate({ onCreate }) {
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    // call onCreate on the title provided; user creates a new book
+    // to call on the submit event for the form
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+    };
+
     return (
-        <div>BookCreate</div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input value={title} onChange={handleChange} />
+                <button>Create</button>
+            </form>
+        </div>
     )
 }
 

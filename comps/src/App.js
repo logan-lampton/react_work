e07b550a37b186d2643c530b470a1c1039ff6ce7
Logan Lampton <lampton.logan@gmail.com>
2023-01-23 17:54:38 -0500

    created propTypes for Button variations

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,22 +1,28 @@
 import Button from './Button';
 
 function App() {
+    
+    // button props
+        // rounded: true or false
+        // outline: true or false
+        // purpose: generally a string that says what the purpose of the button is: "primary, secondary, success, warning, danger."
+
     return (
         <>
         <div>
-            <Button>Click Me!</Button>
+            <Button success rounded outline>Click Me!</Button>
         </div>
         <div>
-            <Button>No Me!</Button>
+            <Button danger outline>No Me!</Button>
         </div>
         <div>
-            <Button>I'm the Button to Click!</Button>
+            <Button warning>I'm the Button to Click!</Button>
         </div>
         <div>
-            <Button>Not Me Please</Button>
+            <Button primary rounded>Not Me Please</Button>
         </div>
         <div>
-            <Button>These buttons, Amiright?!?</Button>
+            <Button danger outline>These buttons, Amiright?!?</Button>
         </div>
         </>
     )
--- comps/src/Button.js
+++ comps/src/Button.js
@@ -1,6 +1,41 @@
+// Using the prop-types library
+// npm install prop-types
+import PropTypes from 'prop-types';
+
 // use the 'children' prop to have anything typed between the buttons appear, including other components like icons
-function Button({ children }) {
+function Button({ 
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded
+}) {
+    if(primary && secondary) {
+        throw new Error('Primary and secondary should not be used on the same element');
+    };
+
     return <button>{children}</button>;
 }
+
+// underneath the component
+// Number() method is built in
+// Number(true) = 1
+// Number(!!undefined) = false / 0
+Button.propTypes = {
+    checkVariationProp: ({ primary, secondary, success, warning, danger }) => {
+        const count = Number(!!primary)
+        + Number(!!secondary)
+        + Number(!!warning)
+        + Number(!! success)
+        + Number(!!danger)
+        
+        if (count > 1) {
+            return new Error('Only one of primary, secondary, success, warning, or danger can be true.')
+        }
+    }
+};
 
 export default Button;

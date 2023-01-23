417c5a9766e511ca8a3a40c7b243fb8ae81c3025
Logan Lampton <lampton.logan@gmail.com>
2023-01-23 18:08:26 -0500

    installed and imported tailwind

---
+++ comps/tailwind.config.js
@@ -0,0 +1,11 @@
+// npm install -D tailwindcss
+// npx tailwindcss init
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

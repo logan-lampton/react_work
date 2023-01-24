87397da3d6c9b992ac30bb2619cd2f54cd72852e
Logan Lampton <lampton.logan@gmail.com>
2023-01-23 19:30:31 -0500

    created CSS rule for button icon margin in input.css file

--- comps/src/Button.js
+++ comps/src/Button.js
@@ -23,7 +23,7 @@ function Button({
     // JS keys can't have a dash (-) unless it is wrapped as a string ('')
     // first argument is any styling that we want for all buttons
     // later classNames override earlier ones
-    const classes = classnames('px-3 py-1.5 border', {
+    const classes = classnames('flex items-center px-3 py-1.5 border', {
         'border-blue-500 bg-blue-500 text-white': primary,
         'border-gray-900 bg-gray-900 text-white': secondary,
         'border-green-500 bg-green-500 text-white': success,

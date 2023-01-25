db34650fd74afa813e00769912a39b2a80091d81
Logan Lampton <lampton.logan@gmail.com>
2023-01-25 11:05:54 -0500

    added Dropdown state, handleClick and map, creating basic functionality

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,9 +1,28 @@
-function Dropdown() {
+import { useState } from 'react';
+
+function Dropdown( {options} ) {
+    // define state
+    const [isOpen, setIsOpen] = useState(false);
+
+    // create event handler
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
+    // map the options prop; use option.value as the key
+    const renderedOptions = options.map((option) => {
+        return (
+            <div key={option.value}>
+                {option.label}
+            </div>
+        )
+    })
+    
     return (
         <>
-            <div>
-                Dropdown...
-            </div>
+            <div onClick={handleClick}>Select...</div>
+            {/* JS Boolean expression; won't render if !isOpen */}
+            {isOpen && <div>{renderedOptions}</div>}
         </>
     )
 }

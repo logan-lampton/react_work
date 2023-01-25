4fddb020eb7aa464d919b157df9cc7e011a99465
Logan Lampton <lampton.logan@gmail.com>
2023-01-25 14:42:46 -0500

    refactored dropdown logic from App to new DropdownPage component, started SideBar component

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,29 +1,14 @@
-import { useState } from 'react';
 import ButtonPage from './pages/ButtonPage';
 import AccordionPage from './pages/AccordionPage';
-import Dropdown from './components/Dropdown';
+import DropdownPage from './pages/DropdownPage';
 
 function App() {
-    const [selection, setSelection] = useState(null);
-
-    // updates selection, based on what the user selects
-    const handleSelect = (option) => {
-        setSelection(option);
-    };
     
-    const options = [
-        {label: 'Red', value: 'red'},
-        {label: 'Green', value: 'green'},
-        {label: 'Blue', value: 'blue'}
-    ];
-
     return ( 
         <>
         <ButtonPage />
         <AccordionPage />
-        <div className="flex">
-            <Dropdown options={options} value={selection} onChange={handleSelect}/>
-        </div>
+        <DropdownPage />
         </>
     )
 }

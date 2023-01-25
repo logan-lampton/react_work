0d04962b131b537398da5d417d46b2a2f265a010
Logan Lampton <lampton.logan@gmail.com>
2023-01-25 15:48:50 -0500

    created Link component, updated App component for functionality to link to different paths within the app

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,6 +1,7 @@
 import ButtonPage from './pages/ButtonPage';
 import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
+import Link from './components/Link';
 
 function App() {
     
@@ -9,6 +10,8 @@ function App() {
         <ButtonPage />
         <AccordionPage />
         <DropdownPage />
+        <Link to="/accordion">Go to accordion</Link>
+        <Link to="/dropdown">Go to dropdown</Link>
         </>
     )
 }
---
+++ comps/src/components/Link.js
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import NavigationContext from '../context/navigation';
+
+// 'to' is the path to navigate to, 'children' is any text to display in the anchor element
+function Link({ to, children }) {
+    // we are grabbing the navigate function from NavigationContext, via useContext and using destructuring to declare it all on one line
+    const { navigate } = useContext(NavigationContext);
+    
+    const handleClick = (event) => {
+        event.preventDefault();
+        navigate(to);
+    };
+    
+    return <a onClick={handleClick}>{children}</a>
+}
+
+export default Link;

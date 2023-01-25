2df2110062e6214f1ad21ad0c8b8899ccabe6514
Logan Lampton <lampton.logan@gmail.com>
2023-01-25 11:35:01 -0500

    Dropdown base behavior in place; added handleSelect, can see the option selected at top of dropdown

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import ButtonPage from './pages/ButtonPage';
 import AccordionPage from './pages/AccordionPage';
 import Dropdown from './components/Dropdown';
 
 function App() {
+    const [selection, setSelection] = useState(null);
+
+    // updates selection, based on what the user selects
+    const handleSelect = (option) => {
+        setSelection(option);
+    };
+    
     const options = [
         {label: 'Red', value: 'red'},
         {label: 'Green', value: 'green'},
@@ -13,7 +21,7 @@ function App() {
         <>
         <ButtonPage />
         <AccordionPage />
-        <Dropdown options={options} />
+        <Dropdown options={options} selection={selection} onSelect={handleSelect}/>
         </>
     )
 }
--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,26 +1,41 @@
 import { useState } from 'react';
 
-function Dropdown( {options} ) {
+function Dropdown( {options, selection, onSelect} ) {
     // define state
     const [isOpen, setIsOpen] = useState(false);
 
-    // create event handler
+    // create event handler for clicking on 'Select...'
     const handleClick = () => {
         setIsOpen(!isOpen);
     };
 
+    // event handler for clicking on an individual option
+    const handleOptionClick = (option) => {
+        // close dropdown
+        setIsOpen(false);
+        // what option was clicked on?
+            // need to pass option from the map below
+                // onClick={() => handleOptionClick(option)}
+        onSelect(option);
+    }
+
     // map the options prop; use option.value as the key
     const renderedOptions = options.map((option) => {
         return (
-            <div key={option.value}>
+            <div onClick={() => handleOptionClick(option)} key={option.value}>
                 {option.label}
             </div>
         )
     })
     
+    let content = 'Select...';
+    if(selection) {
+        content = selection.label;
+    }
+
     return (
         <>
-            <div onClick={handleClick}>Select...</div>
+            <div onClick={handleClick}>{content}</div>
             {/* JS Boolean expression; won't render if !isOpen */}
             {isOpen && <div>{renderedOptions}</div>}
         </>

ecc49a9d7b690256522e4c8cdbf94689e8ef9575
Logan Lampton <lampton.logan@gmail.com>
2023-01-26 19:07:58 -0500

    added ability to toggle Modal on ModalPage

--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import Modal from '../components/Modal';
+import Button from '../components/Button';
 
 function ModalPage() {
+    // set state
+    const [showModal, setShowModal] = useState(false);
+
+    // click event handler
+    const handleClick = () => {
+        setShowModal(true);
+    };
+    
     return (
         <div>
-            <Modal />
+            <Button onClick={handleClick} primary>Open Modal</Button>
+           {/* only show Modal if setModal(true) */}
+           {showModal && <Modal />}
         </div>
     )
 };

e0e4057f7137fbc7d0a2116907177f35447e8644
Logan Lampton <lampton.logan@gmail.com>
2023-01-26 20:02:23 -0500

    made modal fixed CSS and prevented user scrolling on page while the modal is open

--- comps/src/components/Modal.js
+++ comps/src/components/Modal.js
@@ -1,10 +1,21 @@
 import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
 
 function Modal({ onClose, children, actionBar }) {
+    // to prevent scrolling in the modal
+    useEffect(() => {
+        document.body.classList.add('overflow-hidden');
+        // clean-up function
+        return () => {
+            document.body.classList.remove('overflow-hidden');
+        };
+    }, [])
+
     return ReactDOM.createPortal(
         <div>
-            <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
-            <div className="absolute inset-40 p-10 bg-white">
+            {/* fixed className opens modal whereever the user is in the doc */}
+            <div onClick={onClose} className="fixed inset-0 bg-gray-300 opacity-80"></div>
+            <div className="fixed inset-40 p-10 bg-white">
                 <div className="flex flex-col justify-between h-full">
                     {children}
                     <div className={'flex justify-end'}>
--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -23,14 +23,32 @@ function ModalPage() {
     const modal = <Modal onClose={handleClose} actionBar={actionBar}>
         <p>
             This is important! Do you accept that this is important?
-        </p>    
+        </p>
     </Modal>
     
     return (
         <div>
             <Button onClick={handleClick} primary>Open Modal</Button>
            {/* only show Modal if setModal(true) */}
-           {showModal && modal}
+            {showModal && modal}
+            <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>    
+            <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+            <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+            <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+            <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+            <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
         </div>
     )
 };

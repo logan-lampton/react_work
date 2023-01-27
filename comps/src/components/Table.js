1ee5f7233707115de21ae223d930696fe361c111
Logan Lampton <lampton.logan@gmail.com>
2023-01-27 15:20:21 -0500

    mapped data from TablePage to Table

--- comps/src/components/Table.js
+++ comps/src/components/Table.js
@@ -1,6 +1,30 @@
 function Table({ data }) {
+    
+    const fruitData = data.map((fruit) => {
+        return (
+            <tr key={fruit.name}>
+                <td>{fruit.name}</td>
+                <td className={fruit.color}></td>
+                <td>{fruit.score}</td>
+            </tr>
+        );
+    });
+    
     return (
-        <div>{data.length}</div>
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Fruit</th>
+                        <th>Color</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {fruitData}
+                </tbody>
+            </table>
+        </>
     );
 };
 

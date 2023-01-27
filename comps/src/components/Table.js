6c6e3f992e8394680a9b7e9e4ec14b9760158aec
Logan Lampton <lampton.logan@gmail.com>
2023-01-27 16:01:04 -0500

    created dynamic rows for Table

--- comps/src/components/Table.js
+++ comps/src/components/Table.js
@@ -1,20 +1,20 @@
 function Table({ data, config }) {
-    
-    const fruitData = data.map((fruit) => {
-        return (
-            <tr className="border-b" key={fruit.name}>
-                <td className="p-3">{fruit.name}</td>
-                <td className="p-3">
-                    <div className={`p-3 m-2 ${fruit.color}`}></div>
-                </td>
-                <td className="p-3">{fruit.score}</td>
-            </tr>
-        );
-    });
-
+    // render the column headers
     const renderedHeaders = config.map((column) => {
         return (
             <th key={column.label}>{column.label}</th>    
+        );
+    });
+
+    const renderedRows = data.map((fruit) => {
+        // render the data for each row into each column
+        const renderedCells = config.map((column) => {
+            return <td className="p-2" key={column.label}>{column.render(fruit)}</td>
+        });
+        return (
+            <tr className="border-b" key={fruit.name}>
+                {renderedCells}
+            </tr>
         );
     });
     
@@ -27,7 +27,7 @@ function Table({ data, config }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {fruitData}
+                    {renderedRows}
                 </tbody>
             </table>
         </>

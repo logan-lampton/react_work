0c20de797cffd401ebbdb4e38b344231430605e9
Logan Lampton <lampton.logan@gmail.com>
2023-01-27 15:08:57 -0500

    created/linked array of data for Table

--- comps/src/components/Table.js
+++ comps/src/components/Table.js
@@ -1,6 +1,6 @@
-function Table() {
+function Table({ data }) {
     return (
-        <div>Table</div>
+        <div>{data.length}</div>
     );
 };
 
--- comps/src/pages/TablePage.js
+++ comps/src/pages/TablePage.js
@@ -1,9 +1,16 @@
 import Table from '../components/Table';
 
 function TablePage() {
-    return (
+    const data = [
+        { name: 'Orange', color: 'bg-orange-500', score: 3 },
+        { name: 'Apple', color: 'bg-red-500', score: 5 },
+        { name: 'Banna', color: 'bg-yellow-500', score: 4 },
+        { name: 'Lime', color: 'bg-green-500', score: 4 },
+    ];
+
+    return(
         <div>
-            <Table />
+            <Table data={data} />
         </div>
     );
 };

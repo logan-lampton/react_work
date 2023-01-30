0ad374e8fe864194a50b659d01120f098da9ae2b
Logan Lampton <lampton.logan@gmail.com>
2023-01-30 18:27:38 -0500

    adding in logic for sorting ascending and descending in SortableTable

--- comps/src/components/SortableTable.js
+++ comps/src/components/SortableTable.js
@@ -1,20 +1,42 @@
+import { useState } from 'react';
 import Table from './Table';
 
 // take all the props and pass them through to Table.js
 function SortableTable(props) {
+    // by default the table is unsorted
+    const [sortOrder, setSortOrder] = useState(null);
+    const [sortBy, setSortBy] = useState(null);
     const { config } = props;
+
+    const handleClick = (label) => {
+        if(sortOrder === null) {
+            setSortOrder('asc');
+            setSortBy(label);
+        } else if(sortOrder === 'asc') {
+            setSortOrder('desc');
+            setSortBy(label);
+        } else {
+            setSortOrder(null);
+            setSortBy(null);
+        }
+    };
 
     const updatedConfig = config.map((column) => {
         if(!column.sortValue) {
             return column;
         }
         return {
-            ...column, header: () => <th>{column.label} IS SORTABLE</th>
+            ...column, header: () => <th onClick={() => handleClick(column.label)}>{column.label} IS SORTABLE</th>
         };
     });
 
     // by passing in config as a prop AFTER ...props, it will override any differences
-    return <Table {...props} config={updatedConfig} />
+    return (
+        <>
+            {sortOrder} - {sortBy}
+            <Table {...props} config={updatedConfig} />
+        </>
+    )
 };
 
 export default SortableTable;

69e4af5a4cc1519c65f4c3dabbb242c6d218f274
Logan Lampton <lampton.logan@gmail.com>
2023-01-31 16:38:57 -0500

    built out CounterPage to increment and decrement, as well as user being able to submit a value in a form

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -1,15 +1,54 @@
+import { useState } from 'react';
 import Button from '../components/Button';
-import useCounter from '../hooks/use-counter';
+import Panel from '../components/Panel';
 
 function CounterPage({ initialCount }) {
-    const { count, increment } = useCounter(initialCount);
+    const [count, setCount] = useState(initialCount);
+    const [valueToAdd, setValueToAdd] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    const handleChange = (event) => {
+        // might want to use parseFloat instead
+        // OR 0, makes the value 0 if the user doesn't provide one
+        const value = parseInt(event.target.value) || 0;
+        setValueToAdd(value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setCount(count + valueToAdd);
+        setValueToAdd(0);
+    };
 
     return (
-        <div>
-            <h1>Count is {count}</h1>
-            <Button onClick={increment}>Increment</Button>
-        </div>
-    )
-};
+        <>
+            <Panel className="m-3">
+                <h1 className="text-lg">Count is {count}</h1>
+                <div className="flex flex-row">
+                    <Button onClick={increment}>Increment</Button>
+                    <Button onClick={decrement}>Decrement</Button>
+                </div>
+            </Panel>
+
+            <form onSubmit={handleSubmit}>
+                <label>Add a lot!</label>
+                <input 
+                // || "" prevents the input from always having a "0"
+                    value={valueToAdd || ""}
+                    onChange={handleChange}
+                    type="number" 
+                    className="p-1 m-3 bg-gray-50 border border-gray-300" />
+                <Button>Add it!</Button>
+            </form>
+        </>
+    );
+}
 
 export default CounterPage;

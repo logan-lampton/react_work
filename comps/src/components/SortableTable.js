6681ff031c108f6b9f742531348a3dc60312ab72
Logan Lampton <lampton.logan@gmail.com>
2023-01-30 19:23:56 -0500

    sortableTable functionality complete

--- comps/src/components/SortableTable.js
+++ comps/src/components/SortableTable.js
@@ -10,13 +10,19 @@ function SortableTable(props) {
     const { config, data } = props;
 
     const handleClick = (label) => {
+        if(sortBy && label !== sortBy) {
+            setSortOrder('asc');
+            setSortBy(label);
+            return;
+        }
+
         if(sortOrder === null) {
             setSortOrder('asc');
             setSortBy(label);
         } else if(sortOrder === 'asc') {
             setSortOrder('desc');
             setSortBy(label);
-        } else {
+        } else if(sortOrder === 'desc') {
             setSortOrder(null);
             setSortBy(null);
         }
@@ -50,7 +56,8 @@ function SortableTable(props) {
             const valueA = sortValue(a);
             const valueB = sortValue(b);
 
-            const reverseOrder = sortOrder === 'asc' ? 1: -1
+            const reverseOrder = sortOrder === 'asc' ? 1: -1;
+
             if (typeof valueA === 'string') {
                 return valueA.localeCompare(valueB) * reverseOrder;
             } else {

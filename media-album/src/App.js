3b3d552578ff7cb9f19324e52d94ab1226119d95
Logan Lampton <lampton.logan@gmail.com>
2023-03-25 18:47:20 -0400

    created components folder; created Button, Panel, UsersLists components

--- media-album/src/App.js
+++ media-album/src/App.js
@@ -1,5 +1,11 @@
-function App () {
-    return "App!!!"
+import UsersList from "./components/UsersList";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <UsersList />
+    </div>
+  );
 }
 
 export default App;

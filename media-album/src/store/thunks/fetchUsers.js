6de1aec16e4ba3afca85b96ee506e149a308dc1b
Logan Lampton <lampton.logan@gmail.com>
2023-03-27 16:49:23 -0400

    added temporary loading pause for development; created Skeleton.js with initial logic for skeleton loading animation

--- media-album/src/store/thunks/fetchUsers.js
+++ media-album/src/store/thunks/fetchUsers.js
@@ -5,8 +5,19 @@ import axios from "axios";
 // second arguement is an asyn function call to the database
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
+
+  //   DEV ONLY!!!
+  await pause(2000);
+
   return response.data;
 });
+
+// DEV ONLY; remove for final version
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 // exporting the fetchUsers function
 export { fetchUsers };

c8a0f003d911bb2986eda6df58635e9128f5229f
Logan Lampton <lampton.logan@gmail.com>
2023-03-27 19:21:36 -0400

    created reusable thunk hook and refactored rendering user list and creating user code

--- media-album/src/components/UsersList.js
+++ media-album/src/components/UsersList.js
@@ -1,39 +1,29 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
+import { useThunk } from "../hooks/useThunk";
 
 function UsersList() {
-  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-  const [loadingUsersError, setLoadingUsersError] = useState(null);
-  const [isCreatingUser, setIsCreatingUser] = useState(false);
-  const [creatingUserError, setCreatingUserError] = useState(null);
-  const dispatch = useDispatch();
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+  // state below replaced by hook:
+  // const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  // const [loadingUsersError, setLoadingUsersError] = useState(null);
+  // const [isCreatingUser, setIsCreatingUser] = useState(false);
+  // const [creatingUserError, setCreatingUserError] = useState(null);
+  // const dispatch = useDispatch();
   const { data } = useSelector((state) => {
     return state.users;
   });
   useEffect(() => {
-    setIsLoadingUsers(true);
-    // note that dispatch is async in nature, so don't immediately call setIsLoadingUsers(false) afterwards
-    dispatch(fetchUsers())
-      .unwrap()
-      .then()
-      .catch((err) => {
-        setLoadingUsersError(err);
-      })
-      // finally runs after both, so it applies to both states
-      .finally(() => {
-        setIsLoadingUsers(false);
-      });
+    doFetchUsers();
   }, []);
   // event handlers
   const handleUserAdd = () => {
-    setIsCreatingUser(true);
-    dispatch(addUser())
-      .unwrap()
-      .catch((err) => setCreatingUserError(err))
-      .finally(() => setIsCreatingUser(false));
+    doCreateUser();
   };
 
   if (isLoadingUsers) {

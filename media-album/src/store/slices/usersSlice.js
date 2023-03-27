cdbb37b59596f57f97d5fd9c358c7defb7b7288e
Logan Lampton <lampton.logan@gmail.com>
2023-03-27 16:14:38 -0400

    created extraReducers in slice to handle async thunks for users state

--- media-album/src/store/slices/usersSlice.js
+++ media-album/src/store/slices/usersSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const usersSlice = createSlice({
   name: "users",
@@ -7,7 +8,22 @@ const usersSlice = createSlice({
     data: [],
     error: null,
   },
-  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+        // update state object, however appropriate, to show the user what we are loading
+        state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+        // show the user that the request is finished
+        state.isLoading = false;
+        state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+        // show the user that the request failed
+        state.Loading = false;
+        state.error = action.error;
+    });
+  }
 });
 
 export const usersReducer = usersSlice.reducer;

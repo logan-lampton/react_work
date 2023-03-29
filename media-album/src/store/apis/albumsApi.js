ee1dc5607a92a1e5ff15341d660a8a38b3a7a039
Logan Lampton <lampton.logan@gmail.com>
2023-03-29 18:58:14 -0400

    added fetch endpoint to albumsApi

--- media-album/src/store/apis/albumsApi.js
+++ media-album/src/store/apis/albumsApi.js
@@ -1,7 +1,25 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // reducerPath is a propert on the big state object where all of the keys/data of the API state should be maintained
 
 const albumsApi = createApi({
   reducerPath: "albums",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    return {
+      fetchAlbums: builder.query({
+        query: (user) => {
+          return {
+            url: "/albums",
+            params: {
+              userId: user.id,
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
 });

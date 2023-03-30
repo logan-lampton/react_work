65d84af9b0d15c2b95393bc1879b4006650e02cc
Logan Lampton <lampton.logan@gmail.com>
2023-03-30 13:10:15 -0400

    set up albums fetching using redux toolkit in the AlbumsList component

--- media-album/src/store/index.js
+++ media-album/src/store/index.js
@@ -1,13 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { usersReducer } from "./slices/usersSlice";
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    // [] syntax below means to add a key in the object below of what the string is at that path
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
+
+setupListeners(store.dispatch);
 
 // * to export everything from the file
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/deleteUser";
+export { useFetchAlbumsQuery } from "./apis/albumsApi";

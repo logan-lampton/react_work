e6f16f7f7453822dd778e12260f344166629a4fc
Logan Lampton <lampton.logan@gmail.com>
2023-03-30 18:36:47 -0400

    added ability to add photos to albums that show random images/persist in JSON server

--- media-album/src/components/PhotosListItem.js
+++ media-album/src/components/PhotosListItem.js
@@ -1,5 +1,7 @@
-function PhotosListItem() {
-    return "Photos List Item"
+function PhotosListItem({ photo }) {
+    return <div>
+        <img className="h-20 w-20" src={photo.url} alt="random photo" />
+    </div>
 }
 
 export default PhotosListItem;

133783e086f49cd4db02b1e80d86fad63d95a4f8
Logan Lampton <lampton.logan@gmail.com>
2023-03-30 18:14:45 -0400

    created photo api endpoints

--- media-album/src/store/index.js
+++ media-album/src/store/index.js
@@ -2,15 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { usersReducer } from "./slices/usersSlice";
 import { albumsApi } from "./apis/albumsApi";
+import { photosApi } from "./apis/photosApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     // [] syntax below means to add a key in the object below of what the string is at that path
     [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(albumsApi.middleware);
+    return getDefaultMiddleware()
+      .concat(albumsApi.middleware)
+      .concat(photosApi.middleware);
   },
 });
 
@@ -20,4 +24,13 @@ setupListeners(store.dispatch);
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/deleteUser";
-export { useFetchAlbumsQuery, useAddAlbumMutation, useDeleteAlbumMutation } from "./apis/albumsApi";
+export {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useDeleteAlbumMutation,
+} from "./apis/albumsApi";
+export {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useRemovePhotoMutation,
+} from "./apis/photosApi";

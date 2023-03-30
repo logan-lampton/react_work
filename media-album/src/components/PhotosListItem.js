72cd803ccb5db1a3a9af438b3b4f6fe77d00772c
Logan Lampton <lampton.logan@gmail.com>
2023-03-30 18:59:51 -0400

    added functionality/styling to delete a photo

--- media-album/src/components/PhotosListItem.js
+++ media-album/src/components/PhotosListItem.js
@@ -1,7 +1,22 @@
+import { useRemovePhotoMutation } from "../store";
+import { GoTrashcan } from "react-icons/go";
+
 function PhotosListItem({ photo }) {
-    return <div>
-        <img className="h-20 w-20" src={photo.url} alt="random photo" />
+
+    const [removePhoto, results] = useRemovePhotoMutation();
+
+  const handleDeletePhoto = () => {
+    removePhoto(photo);
+  };
+
+  return (
+    <div onClick={handleDeletePhoto} className="relative cursor-pointer m-2">
+      <img className="h-20 w-20" src={photo.url} alt="random photo" />
+      <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+        <GoTrashcan className="text-3xl" />
+      </div>
     </div>
+  );
 }
 
 export default PhotosListItem;
